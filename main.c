59cebc960dede432bb38d4d5837b17cc49cb4523
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-10-31 16:13:18 +0900

    09_06

--- main.c
+++ main.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define ROWS 3
+#define COLS 3
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void printMatrix(int a[][COLS])
+{
+    int i,j;
+    
+    for(i=0;i<ROWS;i++){
+	
+     for(j=0;j<COLS;j++){
+	  printf("%i ",a[i][j]);}
+	 printf("\n"); 
+    }
+}
+
+void addMatrix(int a[][COLS],int b[][COLS],int c[][COLS]){
+	 int i,j;
+    
+    for(i=0;i<ROWS;i++)
+     for(j=0;j<COLS;j++)
+	  c[i][j]=a[i][j]+b[i][j];
+}
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	int grade[SIZE];
-	int score[SIZE];
+	int a [ROWS][COLS]={
+	 {2,3,0},
+	 {8,9,1},
+	 {7,0,5}};
+	int b [ROWS][COLS]={
+	 {1,0,0},
+	 {0,1,0},
+	 {0,0,1}};
+	int c [ROWS][COLS];
 	
-	for(i=0;i<SIZE;i++)
-	 grade[i]=rand()%100;
-	 
-	for(i=0;i<5;i++)
-	 score[i]=grade[i];
-	
-	for(i=0;i<5;i++)
-	 printf("score[%d]=%d\n",i,score[i]);
+	addMatrix(a,b,c);
+	printMatrix(c);
 	
 	return 0;
 }
