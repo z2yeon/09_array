8e347d9982684922b32a19e4a4a5c9f26e5b754c
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-10-31 15:27:08 +0900

    09_03

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	int i;
-	int score[5];
+	int grade[SIZE];
+	int score[SIZE];
 	
-	score[0]=10;
-	score[1]=20;
-	score[2]=30;
-	score[3]=40;
-	score[4]=50;
+	for(i=0;i<SIZE;i++)
+	 grade[i]=rand()%100;
+	 
+	for(i=0;i<5;i++)
+	 score[i]=grade[i];
 	
 	for(i=0;i<5;i++)
 	 printf("score[%d]=%d\n",i,score[i]);
